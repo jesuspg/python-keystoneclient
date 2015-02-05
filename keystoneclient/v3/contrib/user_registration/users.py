2a0f60e70c9ead3cec926c8a83d5b31f72f17f9d
sorube13 <sorube@gmail.com>
2015-02-05 13:44:22 +0100

    user_id saved as parameter

--- keystoneclient/v3/contrib/user_registration/users.py
+++ keystoneclient/v3/contrib/user_registration/users.py
@@ -54,7 +54,8 @@ class UsersManager(base.CrudManager):
             method='PATCH')
 
     def reset_password(self, user, reset_token):
-        url = self.base_url + '/reset_password/{0}/users/{1}'.format(base.getid(reset_token),user['id'])
+        user_id = user['id']
+        url = self.base_url + '/reset_password/{0}/users/{1}'.format(base.getid(reset_token),base.getid(user_id))
         body = {
             'user': user
         }
