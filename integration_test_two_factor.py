2f3517ea0914e7af09ce3e869af26d8bae62e8f9
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-11-23 16:48:33 +0100

    Improved two factor integration test

--- integration_test_two_factor.py
+++ integration_test_two_factor.py
@@ -1,9 +1,8 @@
 from keystoneclient.v3 import client
 from keystoneclient import session
-from keystoneclient.auth.identity import v3
+from keystoneclient.v3.contrib.two_factor import auth
 import pyotp
 
-from keystoneclient.v3.contrib.two_factor import auth
 url = 'http://127.0.0.1:5000/v3'
 
 def fiwareclient(session=None, request=None):# TODO(garcianavalon) use this
@@ -18,11 +17,11 @@ def fiwareclient(session=None, request=None):# TODO(garcianavalon) use this
     # TODO(garcianavalon) find a way to integrate this with the existng keystone api
     # TODO(garcianavalon)caching and efficiency with the client object.
     if not session:
-        session = _two_factor_session(user='idm', password='idm')
+        session = two_factor_session(user='idm', password='idm')
     keystone = client.Client(session=session)
     return keystone
 
-def _two_factor_session(user, password, verification_code=None, domain_id='default'):
+def two_factor_session(user, password, verification_code=None, domain_id='default'):
     auth_object = auth.TwoFactor(auth_url=url,
                                  user_id=user,
                                  password=password,
@@ -30,30 +29,47 @@ def _two_factor_session(user, password, verification_code=None, domain_id='defau
                                  verification_code=verification_code)
     return session.Session(auth=auth_object)
 
-keystone = fiwareclient()
+def enable_two_factor(keystone, user, password):
+    key = keystone.two_factor.keys.generate_new_key(user=user.id, security_question='Who?', security_answer='Me!')
+    print "Created key for example_user: ", key.two_factor_key
 
-# Create example user
-keystone.users.create(name="exampleuser", password="example")
+    if keystone.two_factor.keys.check_activated_two_factor(user=user.id):
+        print "Two factor is enabled for example_user!"
+        code = pyotp.TOTP(key.two_factor_key).now()
+        print code
+        keystone2 = fiwareclient(session=two_factor_session(user=user.id,
+                                                            password=password,
+                                                            verification_code=code))
+        try:
+            keystone2.users.get(user.id)
+            print "Auth worked"
+        except:
+            print "Auth didn't work"
 
-# Enable two factor
-key = keystone.two_factor.keys.generate_new_key(user='exampleuser', security_question='Who?', security_answer='Me!')
-print "Created key for exampleuser.", key.two_factor_key
+def disable_two_factor(keystone, user):
+    keystone.two_factor.keys.deactivate_two_factor(user=user.id)
+    print "Disabling two factor."
 
-if keystone.two_factor.keys.check_activated_two_factor(user='exampleuser'):
-    print "Two factor is enabled for exampleuser!"
-    code = pyotp.TOTP(key.two_factor_key).now()
-    print code
-    keystone2 = fiwareclient(session=_two_factor_session(user="exampleuser",
-                                                         password="exampleuser",
-                                                         verification_code=code))
-    print keystone2.users.list()
+    if not keystone.two_factor.keys.check_activated_two_factor(user=user.id):
+        print "Two factor is disabled for exampleuser!"
 
-# Disable two factor
-keystone.two_factor.keys.deactivate_two_factor(user='exampleuser')
-print "Disabling two factor."
+def main():
+    keystone = fiwareclient()
 
-if not keystone.two_factor.keys.check_activated_two_factor(user='exampleuser'):
-    print "Two factor is disabled for exampleuser!"
+    # Create example user
+    project = keystone.projects.create(name="example_project", domain="default")
+    role = keystone.roles.create(name="example_role")
+    user = keystone.users.create(name="example_user", password="example_user", default_project=project)
+    keystone.roles.grant(role=role, user=user, project=project)
 
-# Delete example user
-keystone.users.delete(user="exampleuser")
+    # Run tests
+    enable_two_factor(keystone, user, password="example_user")
+    disable_two_factor(keystone, user)
+
+    # Delete example user
+    keystone.projects.delete(project)
+    keystone.roles.delete(role)
+    keystone.users.delete(user)
+
+if __name__ == "__main__":
+    main()
