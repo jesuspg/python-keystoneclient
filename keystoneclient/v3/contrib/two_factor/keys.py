a8d47846ad60a8a8be6619ee0065b6106e84982c
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-12-21 17:48:49 +0100

    URLs refactored in two factor auth extension

--- keystoneclient/v3/contrib/two_factor/keys.py
+++ keystoneclient/v3/contrib/two_factor/keys.py
@@ -28,15 +28,24 @@ class KeyManager(base.Manager):
     """Manager class for creating/deleting two factor keys."""
 
     resource_class = Key
-    base_url = '/two_factor_auth'
+    auth_url = '/two_factor_auth'
     security_question_url = '/sec_question'
     two_factor_data_url = '/two_factor_data'
 
     def _url(self, user):
         return '/users/{user_id}'.format(user_id=base.getid(user)) + EXTENSION_PATH
 
-    def _base_url(self, user):
-        return self._url(user) + self.base_url
+    def _auth_url(self, user):
+        return self._url(user) + self.auth_url
+
+    def _security_question_url(self, user):
+        return self._url(user) + self.security_question_url
+
+    def _two_factor_data_url(self, user):
+        return self._url(user) + self.two_factor_data_url
+
+    def _check_base_url(self):
+        return EXTENSION_PATH + self.auth_url
 
     def generate_new_key(self, user, security_question, security_answer):
         if security_question and security_answer:
@@ -48,26 +57,26 @@ class KeyManager(base.Manager):
             data = None
 
         return super(KeyManager, self)._post(body=data, 
-                                             url=self._base_url(user),
+                                             url=self._auth_url(user),
                                              response_key="two_factor_auth")
     
     def deactivate_two_factor(self, user):
-        return super(KeyManager, self)._delete(url=self._base_url(user))
+        return super(KeyManager, self)._delete(url=self._auth_url(user))
 
     def check_activated_two_factor(self, user_id=None, username=None, domain_id=None, domain_name=None):
         try:
             if user_id:
-                super(KeyManager, self)._head(url=EXTENSION_PATH+self.base_url+'?user_id='+user_id)
+                super(KeyManager, self)._head(url=self._check_base_url()+'?user_id='+user_id)
             elif domain_id:
-                super(KeyManager, self)._head(url=EXTENSION_PATH+self.base_url+'?user_name='+username+'&domain_id='+domain_id)
+                super(KeyManager, self)._head(url=self._check_base_url()+'?user_name='+username+'&domain_id='+domain_id)
             elif domain_name:
-                super(KeyManager, self)._head(url=EXTENSION_PATH+self.base_url+'?user_name='+username+'&domain_name='+domain_name)
+                super(KeyManager, self)._head(url=self._check_base_url()+'?user_name='+username+'&domain_name='+domain_name)
             return True
         except:
             return False
 
     def get_two_factor_data(self, user):
-        return super(KeyManager, self)._get(url=self._url(user)+self.two_factor_data_url,
+        return super(KeyManager, self)._get(url=self._two_factor_data_url(user),
                                             response_key="two_factor_auth")
 
     def check_security_question(self, user, security_answer):
@@ -76,4 +85,4 @@ class KeyManager(base.Manager):
         data["two_factor_auth"]["security_answer"] = security_answer
 
         return super(KeyManager, self)._head(body=data,
-                                            url=self._url(user)+self.security_question_url)
+                                            url=self._security_question_url(user))
